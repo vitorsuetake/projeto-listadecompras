8a3bf00b1a6e3f79be0c0b07e9488da860808e41
vitorsuetake <nikero22@hotmail.com>
2025-01-08 13:11:07 -0300

    Curso Finalizado

---
+++ scripts/criarItemDaLista.js
@@ -0,0 +1,42 @@
+import gerarDiaDaSemana from "./gerarDiaDaSemana.js";
+const inputItem = document.getElementById("input-item");
+let contador = 0;
+
+export function criarItemDaLista (){
+    if (inputItem.value === "") {
+        alert("Por favor, insira um item!");
+        return;
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    inputCheckbox.addEventListener("click", function () {
+        if (inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through";
+        } else {
+            nomeItem.style.textDecoration = "none";
+        }
+    });
+
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+
+    const dataCompleta = gerarDiaDaSemana();
+
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+
+    return itemDaLista;
+}
